036ebbc88c34d7ed8f886b5ee1fae107eda3a6ae
Guilherme Henrique Rojas <guilhermehrojas@gmail.com>
2014-09-24 13:09:55 -0300

    Simple Validation for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
+  
+  # The lazy way to permit another attributes for devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up)  do |u|
+      u.permit(
+        :usuario,
+        :empresa,
+        :cnpj,
+        :email,
+        :telefone,
+        :endereco,
+        :tipo_de_negocio,
+        :password)
+    end
+  end
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,4 +3,12 @@ class Usuario < ActiveRecord::Base
   #   :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :usuario,
+            :empresa,
+            :cnpj,
+            :telefone,
+            :endereco,
+            :tipo_de_negocio,
+        presence: true
 end
